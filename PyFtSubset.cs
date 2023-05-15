09bbcfcd7b76dbe6e7580c2a29f7789c18b98c22
Bob le Chat <domth@users.noreply.github.com>
2023-05-15 09:06:36 +0200

    add whitespace and control chars support. (fix #5)

--- PyFtSubset.cs
+++ PyFtSubset.cs
@@ -6,7 +6,6 @@ public class PyFtSubset {
   private readonly FileInfo _fontFile;
   private string _randomFileName = string.Empty;
   private string _text = string.Empty;
-  private string? _unicodes;
 
   public PyFtSubset(FileInfo assFile, FileInfo fontFile) {
     _assFile = assFile;
@@ -22,6 +21,20 @@ public class PyFtSubset {
     return rnd[..rnd.LastIndexOf('.')];
   }
 
+  // a bit more of work than control chars, but still useful
+  // see: https://en.wikipedia.org/wiki/Whitespace_character#Unicode
+  //      https://learn.microsoft.com/en-us/dotnet/api/system.char.iswhitespace?view=net-7.0
+  private static string GetWhiteSpaceChars() {
+    return "U+0009-000D,U+0085,U+0020,U+00A0,U+1680,U+2000-200A,U+2028-2029,U+202F,U+205F,U+3000";
+  }
+
+  // return all control characters (UnicodeCategory.Control)
+  // see: https://en.wikipedia.org/wiki/List_of_Unicode_characters#Control_codes
+  //      https://learn.microsoft.com/en-us/dotnet/api/system.char.iscontrol?view=net-7.0
+  public string ControlChars => "U+0000-001F,U+007F,U+0080-9F";
+
+  public string WhiteSpacesChars => GetWhiteSpaceChars();
+
   public string RandomFileName {
     get { return _randomFileName; }
   }
@@ -31,8 +44,6 @@ public class PyFtSubset {
     // --flavor: format of the output file. Here we use woff2
     // --layout-features: * because we want to keep all OpenType features in our font
     // --text: list of characters to keep from the original text.
-    // --unicodes: comma-separated list of the characters we want in our font.
-    // each character is formatted like this: U+xxxx. ex: A -> 65(dec) -> 41(hex) -> U+0041
     return new string[] {
       $"{_fontFile}",
       "--name-IDs=*",
@@ -40,7 +51,7 @@ public class PyFtSubset {
       "--layout-features=*",
       $"--output-file={_randomFileName}.woff2",
       $"--text={_text}",
-      $"--unicodes={_unicodes}"
+      $"--unicodes={string.Join(',', ControlChars, WhiteSpacesChars)}"
     }; 
   }
 
